e5c10e97bcd0e277c288da654ce6e0d08cb5d548
Alexander Sabanov <alexsab@mail.ru>
2019-09-20 00:10:34 +0400

    css+js
    add progress bar
    fix size uploader

--- js/_custom.js
+++ js/_custom.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	/* */
+	document.querySelectorAll('.resizer-social-option input').forEach(function(element) {
+		element.addEventListener('click', function(event) {
+			document.querySelectorAll('.resizer-social-option input').forEach(function(element) {
+				if(element.checked === false) {
+			        // element.parentNode.classList.remove('checked');
+			        element.parentNode.classList.add('color');
+			    };
+			});
+
+	        // this.parentNode.classList.add('checked');
+	        this.parentNode.classList.remove('color');
+	    });
+	});
+
 
 });
+
+window.onload = function onLoad() {
+  var progressBar = 
+    new ProgressBar.Circle('#progress', {
+      color: '#e91e63',
+      strokeWidth: 50,
+      duration: 2000, // milliseconds
+      easing: 'easeInOut'
+    });
+
+  progressBar.animate(0.8); // percent
+};
